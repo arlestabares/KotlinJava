786657a78b11e5cb17fc091acfdc76be23ea47f9
arlestabares <adtabaresc@uqvirtual.edu.co>
2020-04-15 18:26:21 -0500

    java,kotlin

---
+++ P_Android_Java/Proyecto001/app/src/main/java/com/example/proyecto001/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.proyecto001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText campo1, campo2;
+    TextView etiResultado;
+    int num1, num2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campo1 = findViewById(R.id.et1);
+        campo2 = findViewById(R.id.et2);
+        etiResultado = findViewById(R.id.etiResul);
+    }
+
+    public void onClick(View view) {
+
+        num1 = Integer.parseInt(campo1.getText().toString());
+        num2 = Integer.parseInt(campo2.getText().toString());
+
+        switch (view.getId()) {
+
+            case R.id.btnS:
+                sumar();
+                break;
+            case R.id.btnR:
+                restar();
+                break;
+            case R.id.btnM:
+                multiplicar();
+                break;
+            case R.id.btnD:
+                dividir();
+                break;
+        }
+
+    }
+
+    private void sumar() {
+        int suma = num1 + num2;
+        etiResultado.setText("El resulado de la suma es : " + suma);
+    }
+
+    private void restar() {
+        int resta = num1 - num2;
+        etiResultado.setText("El resultado de la resta es : " + resta);
+    }
+
+    private void multiplicar() {
+        int multiplicacion = num1 * num2;
+        etiResultado.setText("El resultado de la multiplicacion es : " + multiplicacion);
+    }
+
+    private void dividir() {
+
+
+        if (num2 > 0) {
+
+            int dividir = num1 / num2;
+            etiResultado.setText("El resultado de la division es : " + dividir);
+        } else {
+            etiResultado.setText("El valor 2 debe ser mayor a cero para efectuar la division");
+            Toast.makeText(this,"El valor 2 debe ser mayor a cero",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+}
